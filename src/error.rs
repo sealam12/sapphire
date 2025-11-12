e7a911b70ffcdb1428f4244f83c49edd3593b9c0
Matthew Carmichael <sealblox@gmail.com>
2025-11-12 22:01:17 +0000

    Cleaned up error declarations with macro

--- src/error.rs
+++ src/error.rs
@@ -1,46 +1,33 @@
 use std::fmt;
 use std::error::Error;
 
-#[derive(Debug)] // Required for the Error trait
-pub struct ParseError {
-    message: String,
-}
-
-impl ParseError {
-    pub fn new(message: &str) -> Self {
-        ParseError {
-            message: message.to_string(),
+#[macro_export]
+macro_rules! define_error {
+    ($name:ident) => {
+        #[derive(Debug)]
+        // Required for the Error trait
+        pub struct $name {
+            message: String,
         }
-    }
-}
 
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Parse Error: {}", self.message)
-    }
-}
-
-impl Error for ParseError {}
-
-//////////////////////////////////////////////
-
-#[derive(Debug)]
-pub struct ScanError {
-    message: String,
-}
-
-impl ScanError {
-    pub fn new(message: &str) -> Self {
-        ScanError {
-            message: message.to_string(),
+        impl $name {
+            pub fn new(message: &str) -> Self {
+                $name {
+                    message: message.to_string(),
+                }
+            }
         }
-    }
+
+        impl fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "{}", self.message)
+            }
+        }
+        
+        impl Error for $name {}
+    };
 }
 
-impl fmt::Display for ScanError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Parse Error: {}", self.message)
-    }
-}
-
-impl Error for ScanError {}
+define_error!(ParseError);
+define_error!(ScanError);
+define_error!(RuntimeError);
