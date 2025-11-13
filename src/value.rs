714be6a20a8440cb1e1e502eb9595fdf09e7adfa
Matthew Carmichael <sealblox@gmail.com>
2025-11-13 16:03:39 +0000

    Added to_string method for Value

--- src/value.rs
+++ src/value.rs
@@ -5,3 +5,14 @@ pub enum Value {
     Bool(bool),
     Null
 }
+
+impl Value {
+    pub fn to_string(&self) -> String {
+        match self {
+            Value::Str(str) => str.clone(),
+            Value::Number(num) => num.to_string(),
+            Value::Bool(bool) => bool.to_string(),
+            Value::Null => "nil".to_string(),
+        }
+    }
+}
